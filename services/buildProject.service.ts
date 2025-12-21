d2d434b383aa3394ac337e7407611a2493f93450
vedangiitb <vedangbaleiitb@gmail.com>
2025-12-21 10:53:25 +0530

    added cloud build sdk

--- services/buildProject.service.ts
+++ services/buildProject.service.ts
@@ -1,14 +1,30 @@
-import { execSync } from "child_process";
+import { CloudBuildClient } from "@google-cloud/cloudbuild";
 import { JobContext } from "../job/jobContext.js";
+
+const cloudBuild = new CloudBuildClient();
 
 export async function buildProject(ctx: JobContext) {
   const serviceName = `site-${ctx.sessionId}`;
 
-  const cmd = `
-    gcloud builds submit ${ctx.workspace} \
-      --project ${ctx.targetProjectId} \
-      --substitutions=_SERVICE_NAME=${serviceName},_REGION=asia-south1
-  `;
+  const bucketName = ctx.snapshotBucket;
+  const objectName = `projects/${ctx.sessionId}.zip`;
 
-  execSync(cmd, { stdio: "inherit" });
+  const [operation] = await cloudBuild.createBuild({
+    projectId: ctx.targetProjectId,
+    build: {
+      source: {
+        storageSource: {
+          bucket: bucketName,
+          object: objectName,
+        },
+      },
+
+      substitutions: {
+        _SERVICE_NAME: serviceName,
+        _REGION: "asia-south1",
+      },
+    },
+  });
+
+  console.log("Cloud Build triggered:", operation.name);
 }
--- services/deployProject.service.ts
+++ services/deployProject.service.ts
@@ -1,17 +1,30 @@
-import { execSync } from "child_process";
+import { ServicesClient } from "@google-cloud/run";
 import { JobContext } from "../job/jobContext.js";
 import { sendLog } from "../utils/logger.js";
 
+const runClient = new ServicesClient();
+
 export async function deployProject(ctx: JobContext) {
   const serviceName = `site-${ctx.sessionId}`;
+  const region = "asia-south1";
 
-  const url = execSync(
-    `gcloud run services describe ${serviceName} \
-      --project ${ctx.targetProjectId} \
-      --region asia-south1 \
-      --format="value(status.url)"`,
-    { encoding: "utf-8" }
-  ).trim();
+  const servicePath = runClient.servicePath(
+    ctx.targetProjectId,
+    region,
+    serviceName
+  );
+
+  const [service] = await runClient.getService({
+    name: servicePath,
+  });
+
+  console.log(service);
+
+  const url = service.urls?.[0];
+
+  if (!url) {
+    throw new Error("Cloud Run service URL not found");
+  }
 
   sendLog(`DEPLOYED_URL=${url}`);
 }
