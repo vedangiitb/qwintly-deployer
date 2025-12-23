aaf39d0842c99bf37f5a64610e7e2d48829c30fb
vedangiitb <vedangbaleiitb@gmail.com>
2025-12-23 11:29:40 +0530

    supabase error fixing

--- flow/deployer.flow.ts
+++ flow/deployer.flow.ts
@@ -6,17 +6,21 @@ import { makeServicePublic } from "../services/makePublic.service.js";
 import { sendLog } from "../utils/logger.js";
 
 export async function deployerFlow(ctx: JobContext) {
-  await step(ctx, "Building Project", () => buildDeploy(ctx), {
-    retries: 0,
-  });
-
-  await step(ctx, "Updating Access Poilicies", () => makeServicePublic(ctx), {
-    retries: 0,
-  });
-
   await step(ctx, "Sending Details to UI", () => getProjectDetails(ctx), {
     retries: 0,
   });
+
+  // await step(ctx, "Building Project", () => buildDeploy(ctx), {
+  //   retries: 0,
+  // });
+
+  // await step(ctx, "Updating Access Poilicies", () => makeServicePublic(ctx), {
+  //   retries: 0,
+  // });
+
+  // await step(ctx, "Sending Details to UI", () => getProjectDetails(ctx), {
+  //   retries: 0,
+  // });
 
   sendLog("SUCCESS");
 }
