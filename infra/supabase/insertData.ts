ad564f6a74591f928c18c268eea6d39fe0ced266
vedangiitb <vedangbaleiitb@gmail.com>
2025-12-23 10:42:31 +0530

    adding logging supabase

--- infra/supabase/insertData.ts
+++ infra/supabase/insertData.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
+import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL } from "../../config/env.js";
 
 export async function insertDataSupabase(tableName: string, insertData: any) {
+  const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+
+  console.log(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
+  
   const { data, error } = await supabase
     .from(tableName)
     .insert([insertData])
